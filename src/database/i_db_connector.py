1aae6c4f930bf336fb5703cca89fb2deaf8caa94
goncrust <goncalonrua@gmail.com>
2021-02-03 01:09:09 +0000

    register vouchers in movements db

--- src/database/i_db_connector.py
+++ src/database/i_db_connector.py
@@ -9,6 +9,7 @@ movements_db = db_m.MDatabase("src/database/bases/movements.db")
 TRANSFER = 0
 WITHDRAW = 1
 PAYMENT = 2
+VOUCHER = 3
 
 
 def login_user(username, pin):
@@ -119,17 +120,17 @@ def transfer(user_from, iban_to, amount):
     users_db.set_balance(user_to, users_db.get_balance(user_to) + amount)
 
     register_movement(TRANSFER, users_db.get_account_number_from_name(
-        user_from), amount, iban_to, None, None)
+        user_from), amount, iban_to, None, None, None)
 
 
 def payments(user_from, entity, reference, amount):
     users_db.set_balance(user_from, users_db.get_balance(user_from) - amount)
 
     register_movement(PAYMENT, users_db.get_account_number_from_name(
-        user_from), amount, None, entity, reference)
+        user_from), amount, None, entity, reference, None)
 
 
-def register_movement(movement_type, account, amount, account_to, entity, reference):
+def register_movement(movement_type, account, amount, account_to, entity, reference, voucher_type, voucher_code):
     date_now = datetime.now().strftime("%d-%m-%Y %H:%M")
 
     if movement_type == TRANSFER:
@@ -139,3 +140,6 @@ def register_movement(movement_type, account, amount, account_to, entity, refere
     elif movement_type == PAYMENT:
         movements_db.register_payment(
             account, entity, reference, amount, date_now)
+    elif movement_type == VOUCHER:
+        movements_db.register_voucher(
+            account, voucher_type, voucher_code, amount, date_now)
